632e3eb477d497c990468e3c36bbf01dd768cd73
Anurag Dubey <23f1001614@ds.study.iitm.ac.in>
2026-02-15 22:56:47 +0530

    intial

---
+++ frontend/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <HeaderComponent />
+    <main>
+      <p>Welcome to vue</p>
+    </main>
+    <FooterComponent />
+  </div>
+</template>
+
+<script>
+  import HeaderComponent from './components/HeaderComponent.vue';
+  import FooterComponent from './components/FooterComponent.vue';
+
+  export default {
+    name: 'App',
+    components: {
+      HeaderComponent,
+      FooterComponent
+    }
+  };
+  </script>
